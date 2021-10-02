2c92e06aad6184bdac698c65230b490fbeace019
Mikel <zzzvezda24@gmail.com>
2021-10-02 12:28:42 +0300

    added gRPC BCH proj and described proto file

---
+++ BCHGrpcService/Services/BCHGrpcService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+
+using System.Threading.Tasks;
+
+using Serilog;
+
+namespace BCHGrpcService.Services
+{
+    public class BCHGrpcService : BCHGrpc.BCHGrpcBase
+    {
+        private readonly ILogger _logger;
+
+        public BCHGrpcService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<RatingReply> GetRatingByPassport(RatingRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new RatingReply
+            {
+                Rating = 1f
+            });
+        }
+    }
+}
