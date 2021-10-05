5450e7f062496768dfa960b4800fd9433ea01083
Mikel <zzzvezda24@gmail.com>
2021-10-05 14:42:46 +0300

    Added Cashbox service and test for it

---
+++ CashboxGrpcService/Services/CashboxService.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace CashboxGrpcService.Services
+{
+    public class CashboxService : Cashbox.CashboxBase
+    {
+        public override Task<SendMoneyReply> SendMoney(SendMoneyRequest request, ServerCallContext context)
+        {
+            //request with card number unused
+
+            var reply = new SendMoneyReply()
+            {
+                OperationResult = "OK"
+            };
+
+            return Task.FromResult(reply);
+        }
+    }
+}
---
+++ MFOTest/CashboxTests/CashboxTest.cs
@@ -0,0 +1,31 @@
+﻿using CashboxGrpcService;
+using Grpc.Net.Client;
+using Xunit;
+
+namespace MFOTest.CashboxTests
+{
+    public class CashboxTest
+    {
+        private GrpcChannel _channel = GrpcChannel.ForAddress("https://localhost:5001");
+        private Cashbox.CashboxClient _cashboxClient;
+
+        public CashboxTest()
+        {
+            _cashboxClient = new Cashbox.CashboxClient(_channel);
+        }
+
+        [Fact]
+        public void MyTestMethod()
+        {
+            var expected = "OK";
+            var request = new SendMoneyRequest()
+            {
+                CardNumber = "1234 1234 1234 1234"
+            };
+
+            var actual = _cashboxClient.SendMoney(request).OperationResult;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
