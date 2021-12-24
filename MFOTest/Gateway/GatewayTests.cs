36f9696e0058c400c3c90e0778ea37cf248babcd
Mikel <zzzvezda24@gmail.com>
2021-12-24 10:26:52 +0300

    Added new test, deleted irrelevant test

--- MFOTest/Gateway/GatewayTests.cs
+++ MFOTest/Gateway/GatewayTests.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 
 using MongoDB.Bson;
 
@@ -13,30 +15,14 @@ using Xunit;
 
 namespace MFOTest
 {
+    //TODO: по сути, эти тесты являются интеграционными т.к. тестируя gateway мы тестируем и другие микросервисы
     public class GatewayTests
     {
         private readonly HttpClient _httpClient = new HttpClient();
 
         public GatewayTests()
         {
-            _httpClient.BaseAddress = new Uri("https://localhost:44317/api/MFO/");
-        }
-
-        [Fact]
-        public void GETResultMustContainExpectedData()
-        {
-            var expected = File.ReadAllText(@"..\..\..\CreditConveyor\jsonResults\GET_ExpectedResult.json");
-            expected = expected.Replace(Environment.NewLine, "");
-            expected = expected.Replace(" ", "");
-            var body = new StringContent(File.ReadAllText(@"..\..\..\CreditConveyor\jsonBody/GET_body.json"));
-            body.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-
-            var requestResult = _httpClient.PostAsync(_httpClient.BaseAddress, body).Result;
-            var actual = requestResult.Content.ReadAsStringAsync().Result;
-
-            //TODO: check this test
-            //requestResult.EnsureSuccessStatusCode();
-            Assert.Equal(expected, actual);
+            _httpClient.BaseAddress = new Uri("https://localhost:44317/api/MFO");
         }
 
         [Fact]
@@ -44,16 +30,13 @@ namespace MFOTest
         {
             string result;
 
-            using (var request = new HttpRequestMessage(HttpMethod.Get, "/CreditHistory"))
+            using (var request = new HttpRequestMessage(HttpMethod.Get, $"{_httpClient.BaseAddress}/CreditHistory"))
             {
                 request.Headers.Add("PassportNumbers", "1234 123456");
-                //TODO: how to get JSON body from reply?
-                result = _httpClient.Send(request).Content.ReadAsStringAsync().Result;
+                result = _httpClient.Send(request).EnsureSuccessStatusCode().Content.ReadAsStringAsync().Result;
             }
-            Client creditHistory = JsonConvert.DeserializeObject<Client>(result);
 
-            Assert.True(creditHistory.CreditHistory.First().Id == new ObjectId("61bc8b4b5864f85c6b9eb540"));
+            Assert.Contains("61bb03975864f85c6b9eb53f", result);
         }
-
     }
 }
