5c4adf3c6999895c239fe9938fbea0ce7444b534
Mikel <zzzvezda24@gmail.com>
2022-01-01 16:07:54 +0300

    base for new controller

--- CreditConveyorGateWayAPI/Controllers/CashboxController.cs
+++ CreditConveyorGateWayAPI/Controllers/CashboxController.cs
@@ -4,31 +4,32 @@ using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace GatewayAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class CashboxController : ControllerBase
     {
         private readonly ILogger<MFOController> _logger;
+        private readonly IConfiguration _configiration;
 
-        public ValuesController(ILogger<MFOController> logger)
+        public CashboxController(ILogger<MFOController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configiration = configuration;
         }
 
         [HttpGet]
-        [Route("/Method")]
+        [Route("/Balances")]
         [ProducesResponseType(200)]
         [ProducesResponseType(500)]
-        public IActionResult Method
-        (
-            [FromHeader(Name = "Arg")]
-            string arg
-        )
+        public IActionResult GetBalances()
         {
             //TODO: NotImplementedException
+            //send RPC to CashboxService
+            //return JSON with balances
             throw new NotImplementedException();
         }
 
