d7524656a013e15e97e29aafac6fa633f8be22af
Michel R <52571455+Alebardist@users.noreply.github.com>
2022-05-21 18:53:34 +0300

    Added new controller and test for it

---
+++ CreditConveyorGateWayAPI/Controllers/ApplicationsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+using Serilog;
+
+using SharedLib.DTO.Application;
+
+using System.Text;
+using System;
+using RabbitMQ.Client;
+
+using Newtonsoft.Json;
+
+namespace GatewayAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ApplicationsController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+
+        public ApplicationsController(ILogger logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("/api/[controller]/CreateNewApplication")]
+        [Produces("application/json")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(500)]
+        public IActionResult CreateNewApplication([FromBody(EmptyBodyBehavior =
+            Microsoft.AspNetCore.Mvc.ModelBinding.EmptyBodyBehavior.Disallow)]
+            CreditApplication creditApplication)
+        {
+            var factory = new ConnectionFactory() { HostName = _configuration.GetSection("RabbitMQ:Host").Value };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(creditApplication));
+
+                channel.BasicPublish(exchange: _configuration.GetSection("RabbitMQ:ExchangeName").Value,
+                                     routingKey: _configuration.GetSection("RabbitMQ:RoutingKey").Value,
+                                     basicProperties: null,
+                                     body: body);
+            }
+
+            CreditApplication newOne = creditApplication;
+
+            return Created("/CreateNewApplication", creditApplication);
+        }
+    }
+}
---
+++ MFOTest/Gateway/ApplicationControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+using SharedLib.DTO.Application;
+using SharedLib.MongoDB.Implementations;
+
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+using Xunit;
+
+namespace MFOTest.Gateway
+{
+    public class ApplicationControllerTests
+    {
+        private readonly HttpClient _httpClient = new();
+
+        public ApplicationControllerTests()
+        {
+            _httpClient.BaseAddress = new Uri("https://localhost:44317/api/Applications");
+        }
+
+        [Fact]
+        public void CreateNewApplicationMustCreateDocumentInCollection()
+        {
+            CreditApplication creditApplication = new CreditApplication()
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                PassportData = new Passport() { FIO = "123", SeriaAndNumber = "1234" },
+                ContactPhoneNumber = "123",
+                WishedAmount = 40000,
+                TermInDays = 40,
+                MonthIncome = 23000,
+                MonthlyCreditServiceSum = 1,
+                Education = "colledge",
+                JobObject = new Job() { CuratorsWorkPhone = "123", LocalAdress = "addr", OrganizationName = "Company" }
+            };
+
+            var result = _httpClient.PostAsJsonAsync($"{_httpClient.BaseAddress}/CreateNewApplication", creditApplication).Result;
+
+            result.EnsureSuccessStatusCode();
+
+            var collection = MongoDBAccessor<CreditApplication>.GetMongoCollection("CreditApplicationsService",
+                                                                "CreditApplications");
+            var idFromDB = collection.Find(x => x.Id == creditApplication.Id).First().Id;
+
+            Assert.Equal(creditApplication.Id, idFromDB);
+
+            collection.DeleteOne(x => x.Id == creditApplication.Id);
+        }
+    }
+}
