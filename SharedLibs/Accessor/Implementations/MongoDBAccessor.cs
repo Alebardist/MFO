0bbde62680a910a463c7abefc389bf7c3cd370e3
Michel R <52571455+Alebardist@users.noreply.github.com>
2022-05-23 12:27:50 +0300

    Added Connection check method

--- SharedLibs/Accessor/Implementations/MongoDBAccessor.cs
+++ SharedLibs/Accessor/Implementations/MongoDBAccessor.cs
@@ -1,25 +1,30 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+using System.Collections.Generic;
 
 namespace SharedLib.MongoDB.Implementations
 {
     public static class MongoDBAccessor<T>
     {
+        public const string connectionstring = "mongodb://localhost:27017/?readPreference=primary&directConnection=true" +
+                "&connectTimeoutMS=3000&tls=true&socketTimeoutMS=3000&serverSelectionTimeoutMS=3000";
+        private static readonly MongoClient _client = new(connectionstring);
+
         public static IMongoCollection<T> GetMongoCollection(string dbName, string collectionName)
         {
+            CheckConnectionByGettingListOfDBs();
+
             IMongoCollection<T> collection = null;
-            try
-            {
-                MongoClient client = new("mongodb://localhost:27017/?readPreference=primary&directConnection=true" +
-                "&connectTimeoutMS=3000&tls=true&socketTimeoutMS=3000&serverSelectionTimeoutMS=3000");
-                IMongoDatabase mongoDatabase = client.GetDatabase(dbName);
-                collection = mongoDatabase.GetCollection<T>(collectionName);
-            }
-            catch (System.Exception ex)
-            {
-                throw;
-            }
+            IMongoDatabase mongoDatabase = _client.GetDatabase(dbName);
+            collection = mongoDatabase.GetCollection<T>(collectionName);
 
             return collection;
+        }
+
+        public static List<BsonDocument> CheckConnectionByGettingListOfDBs()
+        {
+            return _client.ListDatabases().ToList();
         }
     }
 }
