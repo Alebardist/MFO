fbd238aada890dcbb2e649d2396470c7607d9691
Michel R <52571455+Alebardist@users.noreply.github.com>
2022-06-16 10:50:25 +0300

    Обновлена диаграмма, добавлен новый сервис

--- SharedLibs/Accessor/Implementations/MongoDBAccessor.cs
+++ SharedLibs/Accessor/Implementations/MongoDBAccessor.cs
@@ -8,18 +8,14 @@ namespace SharedLib.MongoDB.Implementations
     public static class MongoDBAccessor<T>
     {
         public const string connectionstring = "mongodb://localhost:27017/?readPreference=primary&directConnection=true" +
-                "&connectTimeoutMS=3000&tls=true&socketTimeoutMS=3000&serverSelectionTimeoutMS=3000";
+                                                "&serverSelectionTimeoutMS=3000";
         private static readonly MongoClient _client = new(connectionstring);
 
         public static IMongoCollection<T> GetMongoCollection(string dbName, string collectionName)
         {
             CheckConnectionByGettingListOfDBs();
-
-            IMongoCollection<T> collection = null;
             IMongoDatabase mongoDatabase = _client.GetDatabase(dbName);
-            collection = mongoDatabase.GetCollection<T>(collectionName);
-
-            return collection;
+            return mongoDatabase.GetCollection<T>(collectionName);
         }
 
         public static List<BsonDocument> CheckConnectionByGettingListOfDBs()
---
+++ SharedLibs/Accessor/Interfaces/IBasicDBOperations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace SharedLib.Accessor.Interfaces
+{
+    internal interface IBasicDBOperations<T>
+    {
+        /// <summary>
+        /// Inserts one record
+        /// </summary>
+        /// <param name="DTO"></param>
+        public void CreateNewRecord(T DTO);
+
+        /// <summary>
+        /// Extracts record according to id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Enumerable object with all found records</returns>
+        public IEnumerable<T> GetRecordByID(string id);
+
+        /// <summary>
+        /// Updates record with passed Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="updatedDTO"></param>
+        public void UpdateRecordByID(string id, T updatedDTO);
+
+        /// <summary>
+        /// Deletes record according to passed id
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteRecordByID(string id);
+    }
+}
